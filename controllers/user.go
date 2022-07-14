ad9cc25acc3fee996ea33ce74fcadfe2f6672897
gebilxs <1799676669@qq.com>
2022-07-14 16:38:06 +0800

    add Acquisition and proofreading of parameters

--- controllers/user.go
+++ controllers/user.go
@@ -1,8 +1,12 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 	"weber/logic"
+	"weber/models"
+
+	"go.uber.org/zap"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,9 +14,30 @@ import (
 //SignUpHandler处理注册请求的函数
 func SignUpHandler(c *gin.Context) {
 	//1.获取参数和参数校验
-
+	//use shouldBindJSON
+	var p models.ParamSignUp
+	if err := c.ShouldBindJSON(&p); err != nil {
+		//如果请求参数有错误，则直接返回相应
+		zap.L().Error("SignUp with invalid params", zap.Error(err)) // zap.String("xx", "vv"),
+		c.JSON(http.StatusOK, gin.H{
+			"msg": "请求参数有错误",
+		})
+		return
+	}
+	//手动对请求参数进行判断要求返回不能为空
+	//手动进行业务规则的校验
+	if len(p.Username) == 0 || len(p.Password) == 0 || len(p.RePassword) == 0 || p.RePassword != p.Password {
+		zap.L().Error("SignUp with invalid params") // zap.String("xx", "vv"),
+		c.JSON(http.StatusOK, gin.H{
+			"msg": "请求参数有错误",
+		})
+		return
+	}
+	fmt.Println(p)
 	//2.业务处理
 	logic.SignUp()
 	//3.返回相应
-	c.JSON(http.StatusOK, "OK")
+	c.JSON(http.StatusOK, gin.H{
+		"msg": "success",
+	})
 }
