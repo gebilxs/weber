e5368f4ee659e550f91e63d981ec9c1487617001
gebilxs <1799676669@qq.com>
2022-07-16 16:53:29 +0800

    fix frontend bug

--- weber_frontend/src/service/api.js
+++ weber_frontend/src/service/api.js
@@ -1,13 +1,14 @@
 
 import axios from 'axios';
-axios.defaults.baseURL = "/api/v1/";
+axios.defaults.baseURL = "/api/";
 axios.interceptors.request.use((config) => {
-  let loginResult = JSON.parse(localStorage.getItem("loginResult"));
-  if (loginResult) { 
-	const token = loginResult.token
-	config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+	let loginResult = JSON.parse(localStorage.getItem("loginResult"));
+	if (loginResult) {
+		const token = loginResult.token
+		config.headers.Authorization = `Bearer ${token}`;
+		config.headers['content-type'] = 'application/json'
+	}
+	return config;
 }, (error) => {
 	return Promise.reject(error);
 });
--- weber_frontend/vue.config.js
+++ weber_frontend/vue.config.js
@@ -2,10 +2,19 @@ module.exports = {
     assetsDir: "static",
     devServer: {
         proxy: {
-            '/api/v1': {
-              target: 'http://127.0.0.1:8081',
-              changeOrigin: true,
-            }
+            // '/api/v1': {
+            //   target: 'http://127.0.0.1:8081',
+            //   changeOrigin: true,
+            // }
+            '/api':{
+              target:'http://127.0.0.1:8081',
+              ws:true,
+              changeOrigin:true,//允许跨域
+              pathRewrite:{
+                  '^/api':''   //请求的时候使用这个api就可以
+              }
+          }
         }
+        
     }
   }
