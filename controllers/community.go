25c4e5b8073a8f16f155f0570c22eb45d20764a1
gebilxs <1799676669@qq.com>
2022-07-17 15:53:58 +0800

    add community

---
+++ controllers/community.go
@@ -0,0 +1,20 @@
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+//----------跟社区相关的----------
+
+func CommunityHandler(c *gin.Context) {
+	//查询到所有的社区（community_id,community_name)以列表的形式
+	data, err := logic.GetCommunity()
+	if err != nil {
+		//打印日志
+		zap.L().Error("logic.GetCommunity() failed", zap.Error(err))
+		ResponseError(c, CodeServerBusy) //不轻易把服务端报错给外界
+		return
+	}
+	ResponseSuccess(c, data)
+}
--- routes/routes.go
+++ routes/routes.go
@@ -24,23 +24,28 @@ func Setup(mode string) *gin.Engine {
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "OK")
 	})
-
+	v1 := r.Group("apv/v1")
 	//注册业务路由
-	r.POST("/signup", controllers.SignUpHandler)
+	v1.POST("/signup", controllers.SignUpHandler)
 
 	//注册登陆业务
-	r.POST("/login", controllers.LoginHandler)
+	v1.POST("/login", controllers.LoginHandler)
+
+	//应用JWT认证的中间件
+	v1.Use(middlewares.JWTAuthMiddleware()){
+		v1.GET("/community",controller.CommunityHandler)
+	}
 
 	//登陆的用户才可以访问
 	//把认证的操作放到中间件里面
-	r.GET("/Ping", middlewares.JWTAuthMiddleware(), func(c *gin.Context) {
-		//如果用户登陆了可以走这一条路由,判断请求头中是否有有效的jwt
-		c.String(http.StatusOK, "pong")
-		////如果用户没有登陆那么用户
-		//c.String(http.StatusOK, "请登录")
-	})
+	//v1.GET("/Ping", middlewares.JWTAuthMiddleware(), func(c *gin.Context) {
+	//	//如果用户登陆了可以走这一条路由,判断请求头中是否有有效的jwt
+	//	c.String(http.StatusOK, "pong")
+	//	////如果用户没有登陆那么用户
+	//	//c.String(http.StatusOK, "请登录")
+	//})
 	//手动关闭接口
-	r.GET("/end", func(c *gin.Context) {
+	v1.GET("/end", func(c *gin.Context) {
 		fmt.Println("手动调接口关闭")
 		setting.Quit <- syscall.Signal(10000000)
 		c.String(http.StatusOK, "OK")
