b6bf5ba4896e69807a70c8f4fe0196e790571ba1
gebilxs <1799676669@qq.com>
2022-07-21 15:40:56 +0800

    add rediskey

---
+++ dao/redis/key.go
@@ -0,0 +1,12 @@
+package redis
+
+//redis key
+
+//redis key 注意使用命名空间的方式,方便查询和拆分
+
+const (
+	KeyPrefix          = "weber"
+	KeyPostTimeZSet    = "post:time"   //zset;帖子发帖时间
+	KeyPostScoreZSet   = "post:score"  //zset;帖子及投票的分数
+	KeyPostVotedPrefix = "post:voted:" //zset;参数是记录用户及投票的类型;参数是post_id
+)
